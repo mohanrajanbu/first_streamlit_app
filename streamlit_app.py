6c709dddd5472f8adea1da8cba66fde4b8eb75c3
mohanraj <116799939+mohanrajanbu@users.noreply.github.com>
2022-10-27 16:04:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Moms New Healthy Diner') 
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(🥣'omega 3 & Blueberry Oatmeal')
+streamlit.text('omega 3 & Blueberry Oatmeal')
 streamlit.text( 🥗 'kale, Spanish & Rocket Smoothie')
 streamlit.text(🐔'Hard-Boiled Free-Range Egg')
 streamlit.text(🥑🍞 'Avocado Toast')
